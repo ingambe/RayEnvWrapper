f136e75c65599ca5a25174500d5f284f0d0ef51d
Pierre Tassel <pierre.tassel@outlook.fr>
2022-01-22 20:16:04 +0100

    long description

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(name='RayEnvWrapper',
       version='1.0.0',
       author="Pierre Tassel",
       author_email="pierre.tassel@aau.at",
       description="OpenAi's gym environment wrapper to vectorize them with Ray",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url="https://github.com/ingambe/RayEnvWrapper",
       packages=find_packages(),
       classifiers=[
